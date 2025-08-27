00a40f5f78d27c920b2646c0d78c3d8300f0940a
Tony Stewers <31263046+TonyStewers@users.noreply.github.com>
2025-08-27 01:06:36 +0000

    Adicionando componente e rotas confirmacao e lista presentes

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { GiftList } from './gift-list/gift-list';
+import { Confirmation } from './confirmation/confirmation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'giftlist',
+    component: GiftList,
+    title: 'Lista de Presentes',
+  },
+  {
+    path: 'confirmation',
+    component: Confirmation,
+    title: 'Confirmação de Presença',
+  },
+];
